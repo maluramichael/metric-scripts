3aa055c8bb200355edcbbd6289c0e304b9f08619
Michael Malura <maluramichael@googlemail.com>
2017-05-10 21:34:00 +0100

    Initialize project

---
+++ disk_space.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+drives=$(df -l -k | grep dev | awk '{print $9}')
+
+for drive in $drives;
+do
+  entry=$(df -k $drive | grep $drive)
+  name=$(echo $entry | awk '{print $1}')
+  used=$(echo $entry | awk '{print $3}')
+  available=$(echo $entry | awk '{print $4}')
+  echo $name.used $used
+  echo $name.available $available
+done
