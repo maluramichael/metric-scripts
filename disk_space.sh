e5c5d71ffef13ebd8f2588259db89bf6b29a6b93
Michael Malura <maluramichael@googlemail.com>
2017-05-11 15:59:59 +0200

    Add disk space script

--- disk_space.sh
+++ disk_space.sh
@@ -5,6 +5,8 @@ drives=$(df -l -k | grep dev | awk '{print $9}')
 for drive in $drives;
 do
   entry=$(df -k $drive | grep $drive)
+  entry=$(echo $entry | sed 's/\/dev\///') # remove /dev/ prefix
+
   name=$(echo $entry | awk '{print $1}')
   used=$(echo $entry | awk '{print $3}')
   available=$(echo $entry | awk '{print $4}')
